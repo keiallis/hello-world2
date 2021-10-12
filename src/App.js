b75e35c8472feddeceb026eac35d99afab9fa358
Keia Watkins <keiallis0429@gmail.com>
2021-10-12 19:17:19 -0400

    hello-world2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Component } from 'react';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Message from './components/Message'; 
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Message from './components/Message'; 
+// import Counter from './components/Counter';
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+// import ParentComponent from './components/ParentComponent';
 
 
 
@@ -16,7 +16,7 @@ class App extends Component{
     render() {
       return (
         <div className= "App">
-          <ParentComponent />
+          {/* <ParentComponent /> */}
           {/* <EventBind /> */}
           {/* <FunctionClick /> 
           <ClassClick /> */}
